89218f0a0baaf6a9eda6e0552f74804637d1c515
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-04 18:42:32 +0300

    started js

---
+++ script.js
@@ -0,0 +1,6 @@
+let modal_home = document.querySelector('.modal-home');
+let logo_box = document.querySelector('.logo-box');
+
+logo_box.addEventListener('click', ()=>{
+    modal_home.classList.add('block')
+})
