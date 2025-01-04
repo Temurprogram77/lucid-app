d4c52f9b863e1b0aefaf443e98d08b0b9340db0c
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-04 18:49:43 +0300

    add classlist toogle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let modal_home = document.querySelector('.modal-home');
 let logo_box = document.querySelector('.logo-box');
+let up = document.querySelector('.up');
 
 logo_box.addEventListener('click', ()=>{
-    modal_home.classList.add('block')
+    modal_home.classList.toggle('block')
+    up.classList.toggle('rotate')
 })
