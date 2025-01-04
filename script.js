36235162c65b63cd8464b2c2ee583ad01bd4cbd5
Temur Coder <temurnarzullayev810@gmail.com>
2025-01-04 18:55:06 +0300

    finish modal-mirror js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let modal_home = document.querySelector('.modal-home');
-let logo_box = document.querySelector('.logo-box');
-let up = document.querySelector('.up');
+let modal_home = document.querySelector(".modal-home");
+let logo_box = document.querySelector(".logo-box");
+let up = document.querySelector(".up");
 
-logo_box.addEventListener('click', ()=>{
-    modal_home.classList.toggle('block')
-    up.classList.toggle('rotate')
-})
+logo_box.addEventListener("click", () => {
+  modal_home.classList.toggle("block");
+  up.classList.toggle("rotate");
+});
